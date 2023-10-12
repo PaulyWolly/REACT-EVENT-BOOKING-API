534f8f7b46250735f28b303e820eb93024fad83a
Paul Welby <pwelby@gmail.com>
2023-10-12 09:44:40 -0700

    Update app.js methods to async and await

--- app.js
+++ app.js
@@ -3,25 +3,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
-const { buildSchema } = require("graphql");
 
-const Event = require("./models/event");
-const User = require("./models/user");
-
-// const graphQlSchema = require('./graphql/schema/index');
-// const graphQlResolvers = require('./graphql/resolvers/index');
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
 
 const isAuth = require('./middleware/is-auth');
-
 const connectDB = require("./config/connectDB");
+
 
 const app = express();
 
-const events = [];
-
 app.use(bodyParser.json());
+
+
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -37,132 +32,10 @@ app.use(isAuth);
 
 // Middleware
 app.use("/graphql", graphqlHttp({
-    schema: buildSchema(`
-
-      type Event {
-        _id: ID!
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-        creator: User!
-      }
-
-      type User {
-        _id: ID!
-        email: String!
-        password: String
-        createdEvents: [Event!]
-      }
-
-      input EventInput {
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-      }
-
-      input UserInput {
-        email: String!
-        password: String!
-      }
-
-      type RootQuery {
-        events: [Event!]!
-      }
-
-      type RootMutation {
-        createEvent(eventInput: EventInput): Event
-        createUser(userInput: UserInput): User
-      }
-
-      schema {
-        query: RootQuery
-        mutation: RootMutation
-      }
-    `),
-    rootValue: {
-      events: () => {
-          return Event
-          .find()
-          .populate('creator')
-          .then(result => {
-            return result.map(event => {
-              return {
-                ...event._doc,
-                _id: event._doc._id.toString(),
-                creator: {
-                  ...event._doc.creator._doc,
-                  _id: event._doc.creator.id
-
-                }
-              };
-            })
-          })
-          .catch(err => {
-            console.log(err);
-          })
-
-        // return ['Romantic cooking', 'Sailing', 'All-night coding'];
-        return events;
-      },
-      createEvent: (args) => {
-
-        const event = new Event({
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: new Date(args.eventInput.date),
-          creator: '6526b58ade084103b70ef6f9'
-        });
-        let createdEvent;
-        return event
-          .save()
-          .then(result => {
-            createdEvent = { ...result._doc, _id: result._doc._id.toString() }
-            return User.findById('6526b58ade084103b70ef6f9')
-          })
-          .then(user => {
-            if (!user) {
-              throw new Error('User not found!');
-            }
-            user.createdEvents.push(event);
-            return user.save();
-          })
-          .then(result => {
-            return createdEvent;
-          })
-          .catch(err => {
-            console.log(err);
-            throw err;
-          });
-      },
-      createUser: args => {
-        return User.findOne({ email: args.userInput.email }).then(user => {
-          if (user) {
-            throw new Error('User exists alraedy!');
-          }
-          return bcrypt.hash(args.userInput.password, 12)
-        })
-        .then(hashedPassword => {
-            const user = new User({
-              email: args.userInput.email,
-              password: hashedPassword
-            });
-            return user.save();
-          })
-          .then(result => {
-            return { ...result._doc, password: null, _id: result.id }
-          })
-          .catch((err) => {
-            throw err;
-          });
-
-      }
-    },
-    graphiql: true
-  })
-);
+  schema: graphQlSchema,
+  rootValue: graphQlResolvers,
+  graphiql: true
+}));
 
 const PORT = process.env.PORT || 8000;
 
@@ -173,10 +46,9 @@ mongoose
     connectDB();
     // Run server
     app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on port ${PORT}`);
     })
   })
   .catch((error) => {
-    console.log(error)}
-  );
-
+    console.log(error)
+  });
--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,11 +1,132 @@
-const authResolver = require('./auth');
-const eventsResolver = require('./events');
-const bookingResolver = require('./booking');
+// const authResolver = require('./auth');
+// const eventsResolver = require('./events');
+// const bookingResolver = require('./booking');
 
-const rootResolver = {
-  ...authResolver,
-  ...eventsResolver,
-  ...bookingResolver
-};
+// const rootResolver = {
+//   ...authResolver,
+//   ...eventsResolver,
+//   ...bookingResolver
+// };
 
-module.exports = rootResolver;
+// module.exports = rootResolver;
+
+const bcrypt = require('bcryptjs');
+
+const Event = require("../../models/event");
+const User = require("../../models/user");
+
+
+const events = async eventIds => {
+  try {
+    const events = await Event.find({
+      _id: {
+        $in: eventIds
+      }
+    })
+    events.map(event => {
+      return {
+        ...event._doc,
+        _id: event.id,
+        date: new Date(event._doc.date).toISOString(),
+        creator: user.bind(this, event.creator)
+      }
+    });
+    return events;
+  } catch (error) {
+    throw error
+  }
+}
+
+const user = async userId => {
+  try {
+    const user = await User.findById(userId)
+    return {
+      ...user._doc,
+      _id: user.id,
+      createdEvents: events.bind(this, user._doc.createdEvents)
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events
+        .map(event => {
+          return {
+            ...event._doc,
+            _id: event.id,
+            date: new Date(event._doc.date).toISOString(),
+            creator: user.bind(this, event._doc.creator)
+          };
+        })
+    } catch (error) {
+      throw error;
+    }
+
+    // return ['Romantic cooking', 'Sailing', 'All-night coding'];
+    return events;
+  },
+  createEvent: async (args) => {
+
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: '65281e9d5f02ff6c18eec8ed'
+    });
+    let createdEvent;
+    try {
+      const result = await event.save();
+      createdEvent = {
+        ...result._doc,
+        _id: result._doc._id.toString(),
+        date: new Date(event._doc.date).toISOString(),
+        creator: user.bind(this, result._doc.creator)
+      }
+      const creator = await User.findById('65281e9d5f02ff6c18eec8ed')
+
+      if (!creator) {
+        throw new Error('User not found!');
+      }
+      creator.createdEvents.push(event);
+      await creator.save();
+
+      return createdEvent;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+
+  },
+  createUser: async args => {
+    try {
+      const existingUser = await User.findOne({
+        email: args.userInput.email
+      })
+      if (existingUser) {
+        throw new Error('User exists already!');
+      }
+      const hashedPassword = await bcrypt.hash(args.userInput.password, 12)
+
+      const user = new User({
+        email: args.userInput.email,
+        password: hashedPassword
+      });
+      const result = await user.save();
+
+      return {
+        ...result._doc,
+        password: null,
+        _id: result.id
+      }
+    } catch (error) {
+      throw error;
+    }
+
+  }
+}
